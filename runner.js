697eab3f3b8fa5bce6152ec576d2379b3f4a4430
Splav <splavacodo56@gmail.com>
2020-09-21 15:16:57 -0600

    Full testing framework implementation

---
+++ runner.js
@@ -0,0 +1,62 @@
+const fs = require('fs'); 
+const path = require('path'); 
+const chalk = require('chalk'); 
+const render = require('./render'); 
+
+const forbiddenDirs = ['node_modules']; 
+class Runner {
+    constructor() { 
+        this.testFiles = []; 
+    }
+
+    async runTests() { 
+        for (let file of this.testFiles) {
+            console.log(chalk.grey(`----- ${file.shortName}`))
+            const beforeEaches = []; 
+            global.beforeEach = (func) => { 
+                beforeEaches.push(func); 
+            }
+
+            global.render = render; 
+
+            global.it = async (desc, func) => { 
+                beforeEaches.forEach(func => func()); 
+                try { 
+                    await func(); 
+                    console.log(chalk.green(`\tOK - ${desc}`)); 
+                } catch (err) { 
+                    const message = err.message.replace(/\n/g, '\n\t\t');
+                    console.log(chalk.red(`\tX - ${desc}`)); 
+                    console.log(chalk.red('\t', message)); 
+                }
+            }; 
+
+            try { 
+                require(file.name); 
+            } catch (err) { 
+                console.log(chalk.red('X - Error Loading File', file.name)); 
+                console.log(err); 
+            }
+        }
+    }
+    
+    async collectFiles(targetPath) { 
+        const files = await fs.promises.readdir(targetPath); 
+        
+        for(let file of files) { 
+            const filepath = path.join(targetPath, file); 
+            const stats = await fs.promises.lstat(filepath);
+
+            if (stats.isFile() && file.includes('.test.js')) { 
+                this.testFiles.push({ name: filepath, shortName: file }); 
+            } else if (stats.isDirectory() && !forbiddenDirs.includes(file)) { 
+                const childFiles = await fs.promises.readdir(filepath); 
+
+                files.push(...childFiles.map(f => path.join(file, f))); //the map will get only the file name to push onto the files array rather than the path
+            }
+        }
+    }
+}
+
+
+module.exports = Runner; 
